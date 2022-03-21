dcbef4740fbf21470fa5a4fa74e6ed160f50e3e2
flaviomaricato@gmail.com <flaviomaricato@gmail.com>
2022-03-21 15:10:19 -0300

    Included loading spinner while waiting for api result

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const newJokeButton = document.querySelector('#js-new-joke');
+const spinner = document.querySelector('#js-spinner');
 
 newJokeButton.addEventListener('click', getJoke);
 
 const endpoint = 'https://api.chucknorris.io/jokes/random';
 
 async function getJoke() {
+    // remove the "hidden" class on the spinner
+    spinner.classList.remove('hidden');
+    // disable the joke button
+    newJokeButton.disabled = true;
+
     // The `try` block executes the statements within it as usual.
     // If an exception is thrown, the statements defined in
     // the `catch` block will be executed.
@@ -21,6 +27,11 @@ async function getJoke() {
     } catch (err) {
         console.log(err);
         alert('Failed to fetch new joke');
+    } finally {
+        // enable the joke button
+        newJokeButton.disabled = false;
+        // add the "hidden" class back again
+        spinner.classList.add('hidden')
     }
 }
 
